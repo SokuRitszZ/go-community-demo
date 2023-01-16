41bdd4f85152a0f752d4b4b7a8247aa1c72a967d
Andrew Leung <1062708758@qq.com>
2023-01-16 17:56:27 +0800

    utils:&logger

--- utils/logger.go
+++ utils/logger.go
@@ -11,7 +11,8 @@ var Logger *log.Logger
 func InitLogger() error {
 	timeStr := time.Now().Format("2006-01-01")
 	filePath := "logs/log-" + timeStr
-	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0744)
+	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0777)
+	defer file.Close()
 	if err != nil {
 		return err
 	}
